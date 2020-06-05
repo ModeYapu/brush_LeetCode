1b2139b027ec409d556166b4de6f976d4d3aef37
ModeYapu <1207565867@qq.com>
2020-06-05 18:24:23 +0800

    array add

--- 2.两数相加.js
+++ 2.两数相加.js
@@ -17,8 +17,20 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var addTwoNumbers = function(l1, l2) {
-    
+var addTwoNumbers = function (l1, l2) {
+  let node = new ListNode("head");
+  let str = node;
+  let add = 0;
+  let sum = 0;
+  while (l1 || l2) {
+    sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + add;
+    str.next = new ListNode(sum % 10); //取余则为新链表的值
+    str = str.next;
+    add = sum >= 10 ? 1 : 0;
+    l1 && (l1 = l1.next);
+    l2 && (l2 = l2.next);
+  }
+  add && (str.next = new ListNode(add));
+  return node.next;
 };
 // @lc code=end
-
