dcd34a1e1f8922a04a8542da092e5071df50f244
ModeYapu <1207565967@qq.com>
2020-07-03 15:48:01 +0800

    tij

--- 62.不同路径.js
+++ 62.不同路径.js
@@ -11,15 +11,23 @@
  * @return {number}
  */
 var uniquePaths = function (m, n) {
-  function factorial(i, a) {
-    a = a || 1;
-    if (i < 2) {
-      return a;
-    }
-    // return factorial(i-1,a*i);
-    return factorial(i - 1, a * i);
-  }
-  let num = factorial(m + n - 2) / (factorial(m - 1) * factorial(n - 1));
-  return num;
+  // function factorial(i, a) {
+  //   a = a || 1;
+  //   if (i < 2) {
+  //     return a;
+  //   }
+  //   // return factorial(i-1,a*i);
+  //   return factorial(i - 1, a * i);
+  // }
+  // let num = factorial(m + n - 2) / (factorial(m - 1) * factorial(n - 1));
+  // return num;
+  let j = n + m - 2;
+  let k = m - 1;
+  let arr = [...Array(k + 1).keys()];
+  arr.shift();
+  let result = arr.reduce((cur, pre) => {
+    return (cur * (j - k + pre)) / pre;
+  }, 1);
+  return result;
 };
 // @lc code=end
