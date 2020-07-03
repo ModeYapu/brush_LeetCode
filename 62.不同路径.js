5eff1554de18e0d9a80257d643ce41d8a47166bc
ModeYapu <1207565967@qq.com>
2020-07-03 15:21:11 +0800

    bugon

---
+++ 62.不同路径.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=62 lang=javascript
+ *
+ * [62] 不同路径
+ */
+
+// @lc code=start
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function (m, n) {
+  function factorial(i, a) {
+    a = a || 1;
+    if (i < 2) {
+      return a;
+    }
+    // return factorial(i-1,a*i);
+    return factorial(i - 1, a * i);
+  }
+  let num = factorial(m + n - 2) / (factorial(m - 1) * factorial(n - 1));
+  return num;
+};
+// @lc code=end
