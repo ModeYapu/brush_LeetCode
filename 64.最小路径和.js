3865109ba009f2aed276ff4275e9544a3da5d32c
ModeYapu <1207565967@qq.com>
2020-07-06 21:37:52 +0800

    急急急

---
+++ 64.最小路径和.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=64 lang=javascript
+ *
+ * [64] 最小路径和
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var minPathSum = function (grid) {
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      let num = grid[i][j];
+      let top = grid[i - 1] ? grid[i - 1][j] : 0;
+      let left = grid[i][j - 1] ? grid[i][j - 1] : 0;
+      if (!i) {
+        top = left;
+      }
+      if (!j) {
+        left = top;
+      }
+      grid[i][j] = num + left >= num + top ? num + top : num + left;
+    }
+  }
+  return grid[grid.length - 1][grid[0].length - 1];
+};
+// @lc code=end
