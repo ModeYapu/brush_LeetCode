cdd558236bfa8859651a1b8389c55cc3fe89ed3a
ModeYapu <1207565967@qq.com>
2020-08-09 21:43:23 +0800

    tij

---
+++ 86.分隔链表.js
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=86 lang=javascript
+ *
+ * [86] 分隔链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} x
+ * @return {ListNode}
+ */
+var partition = function (head, x) {
+  const listNode = new ListNode(0);
+  listNode.next = head;
+  let before = new ListNode(0);
+  let after = new ListNode(0);
+  let smallPoint = before;
+  let bigPoint = after;
+  let cur = listNode.next;
+  while (cur) {
+    if (cur.val >= x) {
+      bigPoint.next = cur;
+      bigPoint = bigPoint.next;
+    } else {
+      smallPoint.next = cur;
+      smallPoint = smallPoint.next;
+    }
+    cur = cur.next;
+  }
+  bigPoint.next = null;
+  smallPoint.next = after.next;
+  return before.next;
+};
+// @lc code=end
